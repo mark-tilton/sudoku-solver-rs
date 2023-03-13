236f40dae75c90034ddba24c4a62cba4d38fea1c
Mark Tilton <mark.tilton.a@gmail.com>
2023-03-12 22:48:26 -0700

    Add naked single detection

--- src/board.rs
+++ src/board.rs
@@ -6,34 +6,47 @@ pub enum Direction {
 }
 
 pub struct Board {
+    // Row major representation of all the board's values
+    // (A list of rows, row first then column, dum dum)
     cells: [[u8; 9]; 9],
+    solve_steps: Vec<SolveStep>,
+}
+
+struct SolveStep {
+    pos: [usize; 2],
+    val: u8,
 }
 
 impl Board {
     pub fn from_cells(cells: [[u8; 9]; 9]) -> Self {
-        Board { cells }
+        Board {
+            cells,
+            solve_steps: Vec::new(),
+        }
     }
 
     // This may only be used to verify if the box is valid.
     // If so, this should probably be refactored to just to that here.
-    fn get_box(&self, x: usize, y: usize) -> [u8; 9] {
-        // Get the top left cell
-        let top = (y as f32 / 3f32).floor() as usize * 3;
-        let left = (x as f32 / 3f32).floor() as usize * 3;
+    fn get_box(&self, col: usize, row: usize) -> [u8; 9] {
+        let cells = self.get_solved_cells();
+        // Get the top left cell of the box
+        let top = (row as f32 / 3f32).floor() as usize * 3;
+        let left = (col as f32 / 3f32).floor() as usize * 3;
         let mut vals = [0; 9];
         for i in 0..3 {
             for j in 0..3 {
-                vals[i * 3 + j] = self.cells[i + top][j + left];
+                vals[i * 3 + j] = cells[j + top][i + left];
             }
         }
         vals
     }
 
     fn get_line(&self, pos: usize, dir: Direction) -> [u8; 9] {
+        let cells = self.get_solved_cells();
         let mut vals = [0; 9];
         for i in 0..9 {
-            vals[i] = self.cells[if let Direction::X = dir { i } else { pos }]
-                [if let Direction::Y = dir { i } else { pos }];
+            vals[i] = cells[if let Direction::X = dir { pos } else { i }]
+                [if let Direction::Y = dir { pos } else { i }];
         }
         vals
     }
@@ -71,20 +84,84 @@ impl Board {
         true
     }
 
-    pub fn display(&self) {
-        for i in 0..9 {
-            for j in 0..9 {
-                let val = self.cells[j][i];
+    fn find_naked_single(&self) -> Option<SolveStep> {
+        let cells = self.get_solved_cells();
+        for row in 0..9 {
+            'nums: for col in 0..9 {
+                if cells[row][col] != 0 {
+                    continue;
+                }
+                let box_vals = self.get_box(col, row);
+                let row_vals = self.get_line(row, Direction::X);
+                let col_vals = self.get_line(col, Direction::Y);
+                // println!("Box vals: {:?}", box_vals);
+                // println!("Row vals: {:?}", row_vals);
+                // println!("Col vals: {:?}", col_vals);
+                // return None;
+                let mut valid_num = 0;
+                for num in 1..10 {
+                    if !box_vals.contains(&num)
+                        && !row_vals.contains(&num)
+                        && !col_vals.contains(&num)
+                    {
+                        if valid_num != 0 {
+                            continue 'nums;
+                        }
+                        valid_num = num;
+                    }
+                }
+                if valid_num != 0 {
+                    return Some(SolveStep {
+                        pos: [row, col],
+                        val: valid_num,
+                    });
+                }
+            }
+        }
+        return None;
+    }
+
+    pub fn solve(&mut self) {
+        loop {
+            let step = self.find_naked_single();
+            if let Some(step) = step {
+                // println!("Found {} at ({}, {})", step.val, step.pos[0], step.pos[1]);
+                // println!("Valid: {}", self.check_valid());
+                // self.display(true);
+                self.solve_steps.push(step);
+                continue;
+            }
+            break;
+        }
+    }
+
+    pub fn get_solved_cells(&self) -> [[u8; 9]; 9] {
+        let mut cells = self.cells;
+        for step in &self.solve_steps {
+            cells[step.pos[0]][step.pos[1]] = step.val;
+        }
+        return cells;
+    }
+
+    pub fn display(&self, solved: bool) {
+        let cells = if solved {
+            self.get_solved_cells()
+        } else {
+            self.cells
+        };
+        for row in 0..9 {
+            for col in 0..9 {
+                let val = cells[row][col];
                 if val == 0 {
                     print!(" ");
                 } else {
                     print!("{}", val);
                 }
-                if j % 3 == 2 && j != 8 {
+                if col % 3 == 2 && col != 8 {
                     print!("|");
                 }
             }
-            if i % 3 == 2 && i != 8 {
+            if row % 3 == 2 && row != 8 {
                 println!("");
                 print!("-----------");
             }
